331d685eacfd614865a774bf17e8881c1229f61c
WolfFire404 <reillyvas9@live.nl>
2017-07-17 17:42:51 +0200

    Commit 11
    Voor nu af wat ik moet hebben.
    Camera movement toegevoegd, turret selection begin gemaakt.
    
    Van plan om verder te gaan in mijn vakantie

---
+++ Assets/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    BuildManager buildManager;
+    
+    void Start ()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void PurchaseStandardTurret ()
+    {
+        Debug.Log("Standard turret purchased");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    } 
+
+    public void PurchaseAnotherTurret ()
+    {
+        Debug.Log("Another turret purchased");
+        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+    }
+}
